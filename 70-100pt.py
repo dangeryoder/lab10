5e623d4144e2860968ca8a3ab6ee9d4d2486b4dc
dangeryoder <dangeryoder@gmail.com>
2014-10-20 15:57:22 -0500

    window lines

--- 70-100pt.py
+++ 70-100pt.py
@@ -20,16 +20,31 @@ root = Tk()
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 
-# house and roof
+# house & roof
 square = drawpad.create_rectangle(200,200,500,500)
 roofline1 = drawpad.create_line(180,220,350,50)
 roofline2 = drawpad.create_line(350,50,520,220)
 
-# windows and door
+# windows & door
 window1 = drawpad.create_rectangle(230,230,310,310)
+window1line1 = drawpad.create_line(230,270,310,270)
+window1line2 = drawpad.create_line(270,230,270,310)
 window2 = drawpad.create_rectangle(470,230,390,310)
-window3 = drawpad.create_rectangle(230,390,310,470) 
+window2line1 = drawpad.create_line(470,270,390,270)
+window2line2 = drawpad.create_line(430,230,430,310)
+window3 = drawpad.create_rectangle(230,390,310,470)
+window3line1 = drawpad.create_line(270,390,270,470)
+window3line2 = drawpad.create_line(230,430,310,430) 
 window4 = drawpad.create_rectangle(470,390,390,470)
+window4line1 = drawpad.create_line(430,390,430,470)
+window4line2 = drawpad.create_line(390,430,470,430)
+door = drawpad.create_rectangle(320,500,380,400)
+
+# handle & chimney
+handle = drawpad.create_oval(330,460,340,450)
+chimney1 = drawpad.create_line(440,60,440,140)
+chimney2 = drawpad.create_line(440,60,480,60)
+chimney3 = drawpad.create_line(480,60,480,180)
 
 
 root.mainloop()
